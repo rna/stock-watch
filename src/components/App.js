c3e42ba411abbdc4113237937761ddde5b6da124
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-25 00:21:38 +0530

    add basic file structure of the app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockComponent from './StockComponent';
+
+function App() {
+  return (
+    <div>
+        <StockComponent />
+    </div>
+  );
+}
+
+export default App;
