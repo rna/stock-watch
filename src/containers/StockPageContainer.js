b1c09fdbecf5489f460fd4606cc6769035befadc
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-28 15:42:31 +0530

    add news component with demo data

--- src/containers/StockPageContainer.js
+++ src/containers/StockPageContainer.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import StockInfoComponent from '../components/StockInfoComponent';
+import NewsComponent from '../components/NewsComponent';
 import demoStockInfo from '../api/demoStockInfo';
+import demoStockNews from '../api/demoStockNews';
 
 const StockPageContainer = () => (
-  <table>
-    <thead>
-      <tr>
-        <th colSpan="5">Statistics (Company Name)</th>
-      </tr>
-    </thead>
-    <StockInfoComponent stockInfo={demoStockInfo[0]} />
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan="5">Statistics</th>
+        </tr>
+      </thead>
+      <StockInfoComponent stockInfo={demoStockInfo[0]} />
+    </table>
+    <div>
+      {demoStockNews.map(news => (
+        <NewsComponent key={demoStockNews.indexOf(news)} news={news} />
+      ))}
+    </div>
+  </div>
 );
 
 export default StockPageContainer;
