88f5c4d03407b44c9eaee9c48c17f36d65431c8e
Ramesh <rameshnaidu.allu@gmail.com>
2020-10-28 16:18:08 +0530

    add company info component with demo data

---
+++ src/components/CompanyInfoComponent.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CompanyInfoComponent = ({ profile }) => (
+  <tbody>
+    <tr>
+      <td><img src={profile.image} alt={profile.symbol} /></td>
+      <td>{profile.companyName}</td>
+    </tr>
+    <tr>
+      <td>Description</td>
+      <td>{profile.description}</td>
+    </tr>
+    <tr>
+      <td>CEO</td>
+      <td>{profile.ceo}</td>
+    </tr>
+    <tr>
+      <td>Industry</td>
+      <td>{profile.industry}</td>
+    </tr>
+    <tr>
+      <td>Website</td>
+      <td>{profile.website}</td>
+    </tr>
+    <tr>
+      <td>Full-time Employees</td>
+      <td>{profile.fullTimeEmployees}</td>
+    </tr>
+    <tr>
+      <td>Address</td>
+      <td>
+        {profile.address}
+        ,
+        {' '}
+        {profile.city}
+        ,
+        {' '}
+        {profile.state}
+        ,
+        {' '}
+        {profile.country}
+      </td>
+    </tr>
+    <tr>
+      <td>Trade Exchange</td>
+      <td>{profile.exchange}</td>
+    </tr>
+  </tbody>
+);
+
+CompanyInfoComponent.propTypes = {
+  profile: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    symbol: PropTypes.string.isRequired,
+    companyName: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    ceo: PropTypes.string.isRequired,
+    industry: PropTypes.string.isRequired,
+    website: PropTypes.string.isRequired,
+    fullTimeEmployees: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+    exchange: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CompanyInfoComponent;
--- src/containers/StockPageContainer.js
+++ src/containers/StockPageContainer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import StockInfoComponent from '../components/StockInfoComponent';
 import NewsComponent from '../components/NewsComponent';
+import CompanyInfoComponent from '../components/CompanyInfoComponent';
 import demoStockInfo from '../api/demoStockInfo';
 import demoStockNews from '../api/demoStockNews';
+import demoCompanyInfo from '../api/demoCompanyInfo';
 
 const StockPageContainer = () => (
   <div>
+    <CompanyInfoComponent profile={demoCompanyInfo[0]} />
     <table>
       <thead>
         <tr>
